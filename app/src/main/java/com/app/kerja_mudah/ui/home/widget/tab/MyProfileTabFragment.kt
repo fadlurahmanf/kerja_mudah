567fef53747f92ef89d3422885533a1e0a6bf747
tffaj <taufik.fajari@akarinti.tech>
2022-09-20 19:31:54 +0700

    update main

--- app/src/main/java/com/app/kerja_mudah/ui/home/widget/tab/MyProfileTabFragment.kt
+++ app/src/main/java/com/app/kerja_mudah/ui/home/widget/tab/MyProfileTabFragment.kt
@@ -160,9 +160,9 @@ class MyProfileTabFragment : BaseFragment<FragmentMyProfileTabBinding>(FragmentM
         component.inject(this)
     }
 
-    private fun testBranchStaging(){
-        // TES BRANCH STAGING
-        // TES BRANCH STAGING
-        // TES BRANCH STAGING
+    private fun testBranchMain(){
+        // TES BRANCH Main
+        // TES BRANCH Main
+        // TES BRANCH Main
     }
 }
