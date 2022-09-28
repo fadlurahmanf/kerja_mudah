9b68564539450399320fe33cc8732498858c5c9d
tffaj <taufik.fajari@akarinti.tech>
2022-09-28 13:57:42 +0700

    create layout for job detail activity

---
+++ app/src/main/java/com/app/kerja_mudah/ui/job/JobDetailActivity.kt
@@ -0,0 +1,17 @@
+package com.app.kerja_mudah.ui.job
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.kerja_mudah.base.BaseActivity
+import com.app.kerja_mudah.databinding.ActivityJobDetailBinding
+
+class JobDetailActivity : BaseActivity<ActivityJobDetailBinding>(ActivityJobDetailBinding::inflate) {
+    override fun initSetup() {
+
+    }
+
+
+    override fun inject() {
+
+    }
+}
