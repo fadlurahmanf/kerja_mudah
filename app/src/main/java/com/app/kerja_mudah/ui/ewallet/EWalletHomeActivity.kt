6c0c5ae69733825d3f1984364932884c98bcb477
tffaj <taufik.fajari@akarinti.tech>
2022-10-09 10:16:10 +0700

    create ewallet introduction activity

---
+++ app/src/main/java/com/app/kerja_mudah/ui/ewallet/EWalletHomeActivity.kt
@@ -0,0 +1,16 @@
+package com.app.kerja_mudah.ui.ewallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.kerja_mudah.base.BaseActivity
+import com.app.kerja_mudah.databinding.ActivityEwalletHomeBinding
+
+class EWalletHomeActivity : BaseActivity<ActivityEwalletHomeBinding>(ActivityEwalletHomeBinding::inflate) {
+
+    override fun initSetup() {
+    }
+
+    override fun inject() {
+
+    }
+}
